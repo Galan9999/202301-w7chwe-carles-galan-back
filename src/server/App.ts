018e64f36e393cbd45822fdcdcf4914f5773c1c9
Carles Galan <galan.carles@gmail.com>
2023-02-24 19:43:10 +0100

    Create app and apply cors policy

---
+++ src/server/App.ts
@@ -0,0 +1,11 @@
+import cors from "cors";
+import morgan from "morgan";
+import express from "express";
+
+const app = express();
+
+app.use(cors());
+app.disable("x-powered-by");
+
+app.use(morgan("dev"));
+app.use(express.json());
