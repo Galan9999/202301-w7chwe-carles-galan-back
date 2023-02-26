7229542c2394087bd5f9b9a763fe7134caa86d07
Carles Galan <galan.carles@gmail.com>
2023-02-26 16:56:47 +0100

    Create loginUser function

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,11 @@
+import { JwtPayload } from "jsonwebtoken";
 export interface UserCredentials {
   username: string;
   password: string;
   email: string;
   image: string;
+}
+
+export interface CustomJwtPayload extends JwtPayload {
+  sub: string;
 }
